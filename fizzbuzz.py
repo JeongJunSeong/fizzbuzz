982690515b8fb9adcf6c76566502abe0244406fb
a99117468@gmail.com <a99117468@gmail.com>
2024-12-16 10:42:10 +0900

    added 'fizzbuzz' code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,9 @@
-print('hello')
+for i in range(1, 15):
+    if i%3 == 0:
+        print('fizz')
+    elif i%5 == 0:
+        print('buzz')
+    elif i%15 == 0:
+        print('fizzbuzz')
+    else:
+        print(i)
