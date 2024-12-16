176a2f13dfa69e589fabdb2aa8ab6ef993738622
a99117468@gmail.com <a99117468@gmail.com>
2024-12-16 11:18:14 +0900

    rebase

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,13 @@
-for i in range(1, 17):
-    if i%3 == 0:
+
+for i in range(1, 31):
+    if i%15 == 0:
+        print('fizzbuzz')
+
+    elif i%3 == 0:
         print('fizz')
     elif i%5 == 0:
         print('buzz')
-    elif i%15 == 0:
-        print('fizzbuzz')
+    
     else:
         print(i)
 
